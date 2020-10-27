f62e52e04e3b1a5c21d9336c3030d0bd88b41bee
BatukovSergey <batukov.sergey.front@gmail.com>
2020-10-27 19:51:19 +0300

    toggle

--- vue/src/components/client/NewClient.vue
+++ vue/src/components/client/NewClient.vue
@@ -2,12 +2,13 @@
   .form
     h2 Новый клиент
     Input(:type='"text"' @setValue='client.name = $event' :placeholder='"ФИО"')
+    Toggle(:name='"юр. лицо"' @switch='client.UR = !client.UR')
     Input(:type='"text"' @setValue='client.type = $event' :placeholder='"Тип"')
     Input(:type='"number"' @setValue='client.pasport = $event' :placeholder='"Паспорт"')
     Input(:type='"text"' @setValue='client.adres = $event' :placeholder='"Адрес"')
     Input(:type='"text"' @setValue='client.company = $event' :placeholder='"Компания"')
     Input(:type='"number"' @setValue='client.INN = $event' :placeholder='"ИНН"')
-    Input(:type='"text"' @setValue='client.urAdres = $event' :placeholder='"юр. адрес"')
+    Input(v-if='client.UR' :type='"text"' @setValue='client.urAdres = $event' :placeholder='"юр. адрес"')
     Input(:type='"number"' @setValue='client.invoice = $event' :placeholder='"счет"')
     Input(:type='"text"' @setValue='client.bank = $event' :placeholder='"банк"')
     Input(:type='"number"' @setValue='client.BIC = $event' :placeholder='"БИК"')
@@ -16,17 +17,19 @@
 
 <script>
 import Input from '../ui/Input'
+import Toggle from '../ui/Toggle'
 
 export default {
   name: "NewClient",
   components: {
-    Input
+    Input,
+    Toggle
   },
   data () {
     return {
       client: {
         name: '',
-        type: '',
+        UR: false,
         contacts: [],
         pasrot: '',
         adres: '',
---
+++ vue/src/components/ui/Toggle.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  .toggle(@click='toggleIt()')
+    .toggle_container(:class='{ toggle_contOn: toggle }')
+      .toggle_switch(:class='{ toggle_on: toggle }')
+    .toggle_name {{ name }}
+</template>
+<script>
+export default {
+  name:'Toggle',
+  props: ['name'],
+  data () {
+    return {
+      toggle: false
+    }
+  },
+  methods: {
+    toggleIt () {
+      this.toggle = !this.toggle
+      this.$emit('switch', this.toggle)
+    }
+  },
+}
+</script>
+<style lang="scss">
+@import '../../scss/variables.scss';
+.toggle {
+  cursor: pointer;
+  max-width: max-content;
+  display: flex;
+  align-items: center;
+  &_container {
+    transition: all .3s;
+    position: relative;
+    border: 1px solid $dark_gray;
+    width: 30px;
+    height: 16px;
+    border-radius: 10px;
+    padding: 2px;
+  }
+  &_switch {
+    transition: all .3s;
+    width: 10px;
+    height: 10px;
+    border-radius: 10px;
+    background-color: $dark_gray;
+  }
+  &_name {
+    margin-left: 10px;
+  }
+  &_on {
+    width: 100%;
+    background-color: black;
+  }
+  &_contOn {
+    border-color: black;
+  }
+}
+</style>
