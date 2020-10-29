fc6d63f39aa4940363b531b1b46571a4ed29bc7a
BatukovSergey <batukov.sergey.front@gmail.com>
2020-10-29 18:57:36 +0300

    front update

---
+++ vue/src/components/ui/Icon.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .icon(:class='{icon_big: big}' :style='{margin: margin}')
+    img(:src='require(`@/assets/img/icons/${name}.svg`)')
+</template>
+<script>
+export default {
+  name:'Icon',
+  props: ['name', 'big', 'margin']
+}
+</script>
+<style lang="scss">
+.icon {
+  img {
+    width: 20px;
+    height: 20px;
+  }
+}
+.icon_big {
+  img {
+    width: 40px;
+    height: 40px;
+  }
+}
+</style>
--- vue/src/components/ui/NavPanel.vue
+++ vue/src/components/ui/NavPanel.vue
@@ -4,38 +4,75 @@
       a(
         v-for='(button, i) in buttons'
         :href='button.link'
+        @click='fullScreen()'
         ) {{ button.text }}
+    .nav_right
+      Icon(:name='"notification"' :margin='"0 10px 0 0"')
+      .full(@click='fullScreen()')
+        Icon(:name='"full"')
 </template>
 <script>
+import Icon from './Icon'
+
 export default {
   name:'NavPanel',
+  components: {
+    Icon
+  },
   data () {
     return {
       buttons: [
         {
-          text: '123',
+          text: 'Заказы',
           icon: '123',
-          link: '123'
+          link: '#'
         },
         {
-          text: '123',
+          text: 'Каледарь',
           icon: '123',
-          link: '123'
+          link: '#'
         },
         {
-          text: '123',
+          text: 'Финансы',
           icon: '123',
-          link: '123'
+          link: '#'
         },
         {
-          text: '123',
+          text: 'Прием заявки',
           icon: '123',
-          link: '123'
+          link: '#'
+        },
+        {
+          text: 'Поставщики',
+          icon: '123',
+          link: '#'
+        },
+        {
+          text: 'Аналитика',
+          icon: '123',
+          link: '#'
+        },
+        {
+          text: 'Клиенты',
+          icon: '123',
+          link: '#'
+        },
+        {
+          text: 'Сотрудники',
+          icon: '123',
+          link: '#'
         },
       ]
     }
   },
   methods: {
+    fullScreen () {
+      if (!document.fullscreen) {
+        document.documentElement.requestFullscreen()
+      } else {
+        document.exitFullscreen()
+      }
+    },
     setValue () {
       this.$emit('setValue', this.value)
     }
@@ -45,11 +82,14 @@ export default {
 <style lang="scss">
 @import '../../scss/variables.scss';
 .nav {
-  height: 40px;
+  // height: 40px;
   width: 100vw;
-  padding: 10px;
+  padding: 10px 40px;
   border: 1px solid $light;
   margin-bottom: -1px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   &_list {
     width: 50%;
     display: flex;
@@ -61,6 +101,9 @@ export default {
         color: black;
       }
     }
+  }
+  &_right {
+    display: flex;
   }
 }
 </style>
